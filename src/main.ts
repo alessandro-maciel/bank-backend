45595e01bf6ceaa7df3e233d630f03d6df7d46f2
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-10-23 14:14:50 +0000

    feat: receive and process transaction

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { PixKeyAlreadyExistsErrorFilter } from './pix-keys/filters/pix-key-already-exists.error';
 import { PixKeyGrpcUnknownErrorFilter } from './pix-keys/filters/pix-key-grpc-unknown-error.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,19 @@ async function bootstrap() {
     }),
   );
 
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['host.docker.internal:9094'],
+      },
+      consumer: {
+        groupId: 'transactions-consumer',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { DataSource, Repository } from 'typeorm';
 import {
   Transaction,
   TransactionOperation,
+  TransactionStatus,
 } from './entities/transaction.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BankAccount } from 'src/bank-accounts/entities/bank-account.entity';
+import { ClientKafka } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { CreateTransactionFromAnotherBankAccountDto } from './dto/create-transaction-from-another-bank-account.dto';
+import { PixKey } from 'src/pix-keys/entities/pix-key.entity';
+import { ConfirmTransactionDto } from './dto/confirm-transaction.dto';
 
 @Injectable()
 export class TransactionsService {
@@ -15,14 +21,18 @@ export class TransactionsService {
     private readonly transactionRepository: Repository<Transaction>,
     @InjectRepository(BankAccount)
     private readonly bankAccountRepository: Repository<BankAccount>,
+    @InjectRepository(PixKey)
+    private readonly pixKeyRepository: Repository<PixKey>,
     private readonly dataSource: DataSource,
+    @Inject('KAFKA_SERVICE')
+    private readonly kafkaService: ClientKafka,
   ) {}
 
   async create(
     bankAccountId: string,
     createTransactionDto: CreateTransactionDto,
   ) {
-    return this.dataSource.transaction(async (manager) => {
+    const transaction = await this.dataSource.transaction(async (manager) => {
       const bankAccount = await manager.findOneOrFail(BankAccount, {
         where: { id: bankAccountId },
         lock: { mode: 'pessimistic_write' },
@@ -42,6 +52,19 @@ export class TransactionsService {
 
       return transaction;
     });
+
+    const sendData = {
+      id: transaction.id,
+      accountId: bankAccountId,
+      amount: createTransactionDto.amount,
+      pixkeyto: createTransactionDto.pix_key_key,
+      pixKeyKindTo: createTransactionDto.pix_key_kind,
+      description: createTransactionDto.description,
+    };
+
+    await lastValueFrom(this.kafkaService.emit('transactions', sendData));
+
+    return transaction;
   }
 
   findAll(bankAccountId: string) {
@@ -49,5 +72,82 @@ export class TransactionsService {
       where: { bank_account_id: bankAccountId },
       order: { created_at: 'DESC' },
     });
+  }
+
+  async createFromAnotherBankAccount(
+    input: CreateTransactionFromAnotherBankAccountDto,
+  ) {
+    const transaction = await this.dataSource.transaction(async (manager) => {
+      const pixKey = await manager.findOneOrFail(PixKey, {
+        where: {
+          key: input.pixKeyTo,
+          kind: input.pixKeyKindTo,
+        },
+      });
+
+      const bankAccount = await manager.findOneOrFail(BankAccount, {
+        where: { id: pixKey.bank_account_id },
+        lock: { mode: 'pessimistic_write' },
+      });
+
+      const transaction = await manager.create(Transaction, {
+        related_transaction_id: input.id,
+        amount: input.amount,
+        description: input.description,
+        bank_account_id: bankAccount.id,
+        bank_account_from_id: input.accountId,
+        pix_key_key: input.pixKeyTo,
+        pix_key_kind: input.pixKeyKindTo,
+        operation: TransactionOperation.credit,
+        status: TransactionStatus.completed,
+      });
+
+      await manager.save(transaction);
+
+      bankAccount.balance += transaction.amount;
+      await manager.save(bankAccount);
+    });
+
+    const sendData = {
+      ...input,
+      status: 'confirmed',
+    };
+
+    await lastValueFrom(
+      this.kafkaService.emit('transaction_confirmation', sendData),
+    );
+
+    return transaction;
+  }
+
+  async confirmTransaction(input: ConfirmTransactionDto) {
+    const transaction = await this.transactionRepository.findOneOrFail({
+      where: {
+        id: input.id,
+      },
+    });
+
+    await this.transactionRepository.update(
+      { id: input.id },
+      {
+        status: TransactionStatus.completed,
+      },
+    );
+
+    const sendData = {
+      id: input.id,
+      accountId: transaction.bank_account_id,
+      amount: Math.abs(transaction.amount),
+      pixkeyto: transaction.pix_key_key,
+      pixKeyKindTo: transaction.pix_key_kind,
+      description: transaction.description,
+      status: TransactionStatus.completed,
+    };
+
+    await lastValueFrom(
+      this.kafkaService.emit('transaction_confirmation', sendData),
+    );
+
+    return transaction;
   }
 }
